82178c2a9f637087de6054df3cab5d9ad8fb5922
Nrupaja Vartak <nrupaja@hotmail.com>
2022-10-17 23:53:22 -0700

    instgram

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Nrupaja Vartak on 10/17/22.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
